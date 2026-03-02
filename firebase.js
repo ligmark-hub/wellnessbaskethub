bf2e5e28e9a1caba139948c42750afe03cf5fba6
ligmark-hub <110553238+ligmark-hub@users.noreply.github.com>
2026-03-02 19:01:46 +0800

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,60 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-app.js";
+import { getFirestore, collection, addDoc } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-firestore.js";
+import { getAuth, GoogleAuthProvider, signInWithPopup, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.0/firebase-auth.js";
+
+// YOUR CONFIG
+const firebaseConfig = {
+  apiKey: "AIzaSyBTPmM7CscEENXHIWWj4SnEcYUVuO35wVM",
+  authDomain: "wellness-basket-12465.firebaseapp.com",
+  projectId: "wellness-basket-12465",
+  storageBucket: "wellness-basket-12465.firebasestorage.app",
+  messagingSenderId: "322509208088",
+  appId: "1:322509208088:web:2749b20d0c810993ac616d",
+  measurementId: "G-H74ZKE3BH7"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+const auth = getAuth(app);
+const provider = new GoogleAuthProvider();
+
+document.addEventListener("DOMContentLoaded", () => {
+
+  const form = document.getElementById("feedback");
+  const loginBtn = document.getElementById("loginBtn");
+
+  loginBtn.addEventListener("click", async () => {
+    await signInWithPopup(auth, provider);
+  });
+
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      loginBtn.style.display = "none";
+      form.style.display = "block";
+    } else {
+      form.style.display = "none";
+      loginBtn.style.display = "block";
+    }
+  });
+
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const user = auth.currentUser;
+    if (!user) return alert("Please login first.");
+
+    const name = document.getElementById("name").value;
+    const message = document.getElementById("message").value;
+
+    await addDoc(collection(db, "feedbacks"), {
+      name: name,
+      message: message,
+      userId: user.uid,
+      createdAt: new Date()
+    });
+
+    alert("Feedback submitted!");
+    form.reset();
+  });
+
+});
